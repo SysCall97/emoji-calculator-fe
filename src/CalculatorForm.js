1b613c4ef2d6bd9a2f2bf0b290432d212aef902f
SysCall97 <kazimashry@gmail.com>
2022-06-24 17:44:06 +0600

    handle division by zero error

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -11,9 +11,11 @@ const CalculatorForm = () => {
     const [input1, setInput1] = useState(0);
     const [input2, setInput2] = useState(0);
     const [result, setResult] = useState(0);
+    const [isDivisionByZeroErrorOccured, setIsDivisionByZeroErrorOccured] = useState(false);
     const [operation, setOperation] = useState("+");
     const handleCalculate = () => {
         let val;
+        setIsDivisionByZeroErrorOccured(false);
         switch(operation) {
             case "+": {
                 val = input1 + input2;
@@ -28,12 +30,14 @@ const CalculatorForm = () => {
                 break;
             }
             case "/": {
-                val = input1 / input2;
+                if(input2 === 0) setIsDivisionByZeroErrorOccured(true);
+                else val = input1 / input2;
                 break;
             }
         }
         setResult(val);
     }
+
     return (
         <div>
             <input type="number" value={input1} onInput={ e => setInput1(Number(e.target.value))} />
@@ -43,10 +47,20 @@ const CalculatorForm = () => {
                 }
             </select>
             <input type="number" value={input2} onInput={ e => setInput2(Number(e.target.value))} />
+            {
+                isDivisionByZeroErrorOccured && 
+                <p style={{color: "red"}}>
+                    *Cannot divide by zero
+                </p>
+            }
             <button onClick={handleCalculate}>Calculate</button>
-            <p>
-                {result}
-            </p>
+            {
+                !isDivisionByZeroErrorOccured &&
+                <p>
+                    {result}
+                </p>
+            }
+            
         </div>
     );
 };
