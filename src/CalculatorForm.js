9b12ad2be6647e16b2c45f963c3ce0ed11de7ada
SysCall97 <kazimashry@gmail.com>
2022-06-24 20:59:37 +0600

    API integrated

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -12,34 +12,31 @@ const CalculatorForm = () => {
     const [input1, setInput1] = useState(0);
     const [input2, setInput2] = useState(0);
     const [result, setResult] = useState(0);
-    const [isDivisionByZeroErrorOccured, setIsDivisionByZeroErrorOccured] = useState(false);
     const [operation, setOperation] = useState("+");
+    const [isError, setIsError] = useState(false);
+    const [errorList, setErrorList] = useState([]);
     const handleCalculate = () => {
-        let val;
-        setIsDivisionByZeroErrorOccured(false);
-        switch(operation) {
-            case "+": {
-                val = input1 + input2;
-                break;
+        let status;
+        fetch('http://127.0.0.1:8000/api/calculate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({input1, input2, operation})
+        }).then(async res => {
+            status = res.status;
+            return res.json();
+        }).then(data => {
+            if(status === 200) {
+                setIsError(false);
+                setErrorList([])
+                setResult(data.data);
+            } else {
+                const errors = Object.values(data.errors).map(item => item[0]);
+                setIsError(true);
+                setErrorList(errors);
             }
-            case "-": {
-                val = input1 - input2;
-                break;
-            }
-            case "*": {
-                val = input1 * input2;
-                break;
-            }
-            case "/": {
-                if(input2 === 0) setIsDivisionByZeroErrorOccured(true);
-                else val = input1 / input2;
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-        setResult(val);
+        });
     }
 
     return (
@@ -64,14 +61,16 @@ const CalculatorForm = () => {
             </div>
             <div className='resultArea'>
                 {
-                    !isDivisionByZeroErrorOccured &&
+                    !isError &&
                     <p> Result: {result} </p>
                 }
                 {
-                        isDivisionByZeroErrorOccured && 
-                        <p className='errorMsg'>
-                            *Cannot divide by zero
-                        </p>
+                        isError && 
+                        <ul className='errorMsg'>
+                            {
+                                errorList.map((err, ind) => <li key={ind}>{err}</li>)
+                            }
+                        </ul>
                 }
             </div>
         </div>
